084823e791069ed625f312c614c5cda054edf538
StoneEvens <steven930211@gmail.com>
2024-03-11 02:53:30 +0800

    Important Update

--- app/src/main/java/com/sinopacremit/Remit.java
+++ app/src/main/java/com/sinopacremit/Remit.java
@@ -1,11 +1,12 @@
 package com.sinopacremit;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -15,16 +16,27 @@ public class Remit extends AppCompatActivity {
 
     ExpandableListView storeListView;
 
-    Button confirmButton;
+    CardView confirmCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.remit);
 
+        ArrayList<String> accounts = new ArrayList<String>();
+        ArrayList<String> banks = new ArrayList<String>();
+        accounts.add("111306075");
+        accounts.add("111306111");
+
+
         accountSpinner = findViewById(R.id.AccountSpinner);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(Remit.this, android.R.layout.simple_spinner_dropdown_item, accounts);
+        arrayAdapter.setDropDownViewResource(R.layout.remit_account);
+        accountSpinner.setAdapter(arrayAdapter);
+
+
         storeListView = findViewById(R.id.StoreListView);
-        confirmButton = findViewById(R.id.ConfirmButton);
+        confirmCardView = findViewById(R.id.ConfirmCardView);
 
         ArrayList<String[][]> stores = new ArrayList<String[][]>();
 
@@ -35,5 +47,6 @@ public class Remit extends AppCompatActivity {
         stores.add(temp2);
 
         RemitStoreAdapter storeAdapter = new RemitStoreAdapter(this, stores);
+        storeListView.setAdapter(storeAdapter);
     }
 }
